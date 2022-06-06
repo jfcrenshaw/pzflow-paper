0d2c06024a4d6a8ed58eae98dc0efc9c7739f420
John Franklin Crenshaw <jfcrenshaw@gmail.com>
2022-06-06 15:22:38 -0700

    Added smooth color distribution plot.

---
+++ src/scripts/plot_smooth_color_distribution.py
@@ -0,0 +1,54 @@
+import matplotlib.pyplot as plt
+import pandas as pd
+import paths
+from pzflow import Flow
+
+# load the cosmoDC2 data
+data = pd.read_pickle(paths.data / "cosmoDC2_subset.pkl")
+
+# load the flow
+flow = Flow(file=paths.data / "main_galaxy_flow" / "flow.pzflow.pkl")
+
+# pull out a subset of cosmoDC2 for plotting
+data = data.iloc[:100_000]
+
+# draw a same-sized sample from the flow
+samples = flow.sample(data.shape[0], seed=0)
+
+# create the figure
+fig, (ax1, ax2) = plt.subplots(1, 2, figsize=(6, 2.7), constrained_layout=True, dpi=120)
+
+# set global plot settings
+plot_settings = {
+    "bins": 512,
+    "cmap": "ocean_r",
+    "rasterized": True,
+}
+
+ax1.hist2d(data["redshift"], data["r"] - data["i"], **plot_settings)
+ax2.hist2d(samples["redshift"], samples["r"] - samples["i"], **plot_settings)
+
+# set global axis settings
+ax_settings = {
+    "xlim": (0, 2.5),
+    "ylim": (-0.25, 1.3),
+    "xlabel": "redshift",
+}
+
+# set axis settings
+ax1.set(ylabel="$r - i$", **ax_settings)
+ax2.set(ylabel=" ", **ax_settings)
+
+
+# set the labels
+text_settings = {
+    "x": 0.95,
+    "y": 0.95,
+    "va": "top",
+    "ha": "right",
+}
+ax1.text(s="CosmoDC2", transform=ax1.transAxes, **text_settings)
+ax2.text(s="PZFlow", transform=ax2.transAxes, **text_settings)
+
+# save the figure!
+fig.savefig(paths.figures / "smooth_color_distribution.pdf", dpi=300)
