eae8156bc59aa71b3c00e5702bf3fe74bd6733ff
John Franklin Crenshaw <jfcrenshaw@gmail.com>
2022-11-30 13:31:27 -0800

    Updated appendix on training.

--- src/scripts/photo-z/plot_ensemble_losses.py
+++ src/scripts/photo-z/plot_ensemble_losses.py
@@ -1,22 +1,22 @@
 """Plot the training losses for the pz ensemble."""
-# %%
 import pickle
-from pathlib import Path
 
-import numpy as np
+import matplotlib.pyplot as plt
 from showyourwork.paths import user as Paths
-
-"""
-This currently doesn't work because I accidentally saved the losses as jax floats.
-I need to re-run training and save the losses as regular floats.
-"""
 
 # instantiate the paths
 paths = Paths()
 
-# %%
 # load the losses
 with open(paths.data / "pz_ensemble/losses.pkl", "rb") as file:
     losses = pickle.load(file)
 
-# %%
+# plot the losses
+fig, ax = plt.subplots(figsize=(3, 3), constrained_layout=True)
+for flow, loss in losses.items():
+    ax.plot(loss, alpha=0.5, label=flow)
+ax.legend()
+ax.set(xlabel="Epochs", ylabel="Loss", ylim=(-1.6, 0))
+
+# save the figure
+fig.savefig(paths.figures / "ensemble_losses.pdf")
