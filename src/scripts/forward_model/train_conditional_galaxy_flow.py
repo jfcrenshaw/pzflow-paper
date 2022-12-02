b51391676cabb1e9e6175d79ac711cf840c61286
John Franklin Crenshaw <jfcrenshaw@gmail.com>
2022-12-02 09:27:01 -0800

    Changes to text. Diagram for conditional flow. Jax from conda.

--- src/scripts/forward_model/train_conditional_galaxy_flow.py
+++ src/scripts/forward_model/train_conditional_galaxy_flow.py
@@ -45,13 +45,13 @@ flow = Flow(
 # train for three rounds of 150 epochs
 # after each round, decrease the learning rate by a factor of 10
 opt = optax.adam(1e-3)
-losses = flow.train(train_set, epochs=150, optimizer=opt, seed=0)
+losses = flow.train(train_set, epochs=150, optimizer=opt, seed=0, verbose=True)
 
 opt = optax.adam(1e-4)
-losses += flow.train(train_set, epochs=150, optimizer=opt, seed=1)
+losses += flow.train(train_set, epochs=150, optimizer=opt, seed=1, verbose=True)
 
 opt = optax.adam(1e-5)
-losses += flow.train(train_set, epochs=150, optimizer=opt, seed=2)
+losses += flow.train(train_set, epochs=150, optimizer=opt, seed=2, verbose=True)
 
 # save some info with the model
 flow.info = (
--- src/scripts/forward_model/train_main_galaxy_flow.py
+++ src/scripts/forward_model/train_main_galaxy_flow.py
@@ -56,13 +56,13 @@ flow = Flow(train_set.columns, bijector=bijector)
 # train for three rounds of 50 epochs
 # after each round, decrease the learning rate by a factor of 10
 opt = optax.adam(1e-3)
-losses = flow.train(data, epochs=50, optimizer=opt, seed=0)
+losses = flow.train(data, epochs=50, optimizer=opt, seed=0, verbose=True)
 
 opt = optax.adam(1e-4)
-losses += flow.train(data, epochs=50, optimizer=opt, seed=1)
+losses += flow.train(data, epochs=50, optimizer=opt, seed=1, verbose=True)
 
 opt = optax.adam(1e-5)
-losses += flow.train(data, epochs=50, optimizer=opt, seed=2)
+losses += flow.train(data, epochs=50, optimizer=opt, seed=2, verbose=True)
 
 # save some info with the model
 flow.info = (
